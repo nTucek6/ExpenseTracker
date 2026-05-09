c78b22fb824cf4ae1cb081adceb0544f7e7d2e93
nTucek6 <lordd4193@gmail.com>
2026-05-09 19:25:09 +0200

    Create categories table

--- app/src/main/java/com/example/expensetracker/data/database/ExpenseTrackerDatabase.kt
+++ app/src/main/java/com/example/expensetracker/data/database/ExpenseTrackerDatabase.kt
@@ -1,20 +1,26 @@
 package com.example.expensetracker.data.database
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.expensetracker.data.dao.CacheCrudDao
 import com.example.expensetracker.data.dao.ExpenseDao
 import com.example.expensetracker.data.dao.MonthlySummaryDao
 import com.example.expensetracker.data.entity.CacheCrud
+import com.example.expensetracker.data.entity.Categories
 import com.example.expensetracker.data.entity.Expense
 import com.example.expensetracker.data.entity.MonthlySummary
 import com.example.expensetracker.data.model.BudgetWithSpent
 import com.example.expensetracker.data.model.ExpenseWithGroupSum
 
-@Database(entities = [Expense::class, MonthlySummary::class, CacheCrud::class], [BudgetWithSpent::class, ExpenseWithGroupSum::class] ,version = 2, exportSchema = false)
+@Database(entities = [Expense::class, MonthlySummary::class, CacheCrud::class, Categories::class],
+    [BudgetWithSpent::class, ExpenseWithGroupSum::class] ,version = 2, exportSchema = false,
+)
 @TypeConverters(Converters::class)
 abstract class ExpenseTrackerDatabase : RoomDatabase() {
     abstract fun expenseDao(): ExpenseDao
@@ -24,13 +30,29 @@ abstract class ExpenseTrackerDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: ExpenseTrackerDatabase? = null
+
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("""
+            CREATE TABLE IF NOT EXISTS `categories` (
+                `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                `displayName` TEXT NOT NULL,
+                `imageSvg` INTEGER NOT NULL,
+                `isDefault` INTEGER NOT NULL
+            )
+        """.trimIndent())
+            }
+        }
+
         fun getDatabase(context: Context): ExpenseTrackerDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     ExpenseTrackerDatabase::class.java,
                     "expense_tracker_database"
-                ).build()
+                )
+                    .addMigrations(MIGRATION_1_2)
+                    .build()
                 INSTANCE = instance
                 instance
             }
