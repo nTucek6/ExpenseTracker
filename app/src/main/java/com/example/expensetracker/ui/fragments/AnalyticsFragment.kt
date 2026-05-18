07af1eb7b64b86229a2723a31c1a44e29b19af56
Nikola <lordd4193@gmail.com>
2026-05-18 14:35:57 +0200

    Update xml, created pie chart, need data

--- app/src/main/java/com/example/expensetracker/ui/fragments/AnalyticsFragment.kt
+++ app/src/main/java/com/example/expensetracker/ui/fragments/AnalyticsFragment.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -11,10 +12,14 @@ import com.example.expensetracker.R
 import com.example.expensetracker.data.viewModel.ExpenseViewModel
 import com.example.expensetracker.utils.toDateString
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
 import kotlinx.coroutines.launch
@@ -30,18 +35,33 @@ class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
     private val expenseViewModel: ExpenseViewModel by activityViewModels()
 
     private lateinit var spendingChart: LineChart
-    private lateinit var barData: LineData
-    private lateinit var barDataSet: LineDataSet
+    private lateinit var categorySpendingChart: PieChart
+
+    //private lateinit var barData: LineData
+    //private lateinit var barDataSet: LineDataSet
     private lateinit var spendingData: ArrayList<Entry>
+    private lateinit var categorySpendingData: ArrayList<PieEntry> //value, label su ulazne vrijednosti
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val incTotalSpent: View = view.findViewById(R.id.incTotalSpent)
+        val incTotalIncome: View = view.findViewById(R.id.incTotalIncome)
+        val incBalance: View = view.findViewById(R.id.incBalance)
+        val incAvgDay: View = view.findViewById(R.id.incAvgDay)
+
+        incTotalSpent.findViewById<TextView>(R.id.tvTitle).text = getString(R.string.total_spent)
+        incTotalIncome.findViewById<TextView>(R.id.tvTitle).text = getString(R.string.total_spent)
+        incBalance.findViewById<TextView>(R.id.tvTitle).text = getString(R.string.total_spent)
+        incAvgDay.findViewById<TextView>(R.id.tvTitle).text = getString(R.string.total_spent)
+
         spendingChart = view.findViewById(R.id.lineGraph)
+        categorySpendingChart = view.findViewById(R.id.pieGraph)
         setLineChart()
+        setPieChart()
     }
 
     private fun setLineChart() {
-
         spendingData = ArrayList()
 
         val labels = mutableListOf<String>()
@@ -63,13 +83,11 @@ class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
 
         lifecycleScope.launch {
             val data =
-                expenseViewModel.getDailyBudgetSpent(firstDayOfMonthMillis,lastDayOfMonthMillis)
-            Log.d("Analytics", data.toString())
+                expenseViewModel.getDailyBudgetSpent(firstDayOfMonthMillis, lastDayOfMonthMillis)
             data.forEachIndexed { index, item ->
                 spendingData.add(Entry(index.toFloat(), item.amount.toFloat()))
                 labels.add(item.date.toDateString())
             }
-            Log.d("Analytics", spendingData.toString())
 
             if (spendingData.isNotEmpty()) {
                 val decimalFormat = DecimalFormat("0.00")
@@ -107,4 +125,52 @@ class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
         }
     }
 
+    private fun setPieChart() {
+
+        lifecycleScope.launch {
+            /* val data =
+                expenseViewModel.getDailyBudgetSpent(firstDayOfMonthMillis,lastDayOfMonthMillis)
+            data.forEachIndexed { index, item ->
+                spendingData.add(Entry(index.toFloat(), item.amount.toFloat()))
+                labels.add(item.date.toDateString())
+            } */
+
+            categorySpendingData = listOf(
+                PieEntry(50f, "Food"),
+                PieEntry(50f, "Bills"),
+                PieEntry(50f, "Other")
+            ) as ArrayList<PieEntry>
+
+            if (spendingData.isNotEmpty()) {
+                val decimalFormat = DecimalFormat("0.00")
+                val pieDataSet = PieDataSet(categorySpendingData, "Data set")/*.apply {
+                    setDrawValues(true)
+                    valueTextSize = 10f
+                    valueTextColor = Color.WHITE
+                    valueFormatter = object : ValueFormatter() {
+                        override fun getPointLabel(entry: Entry?): String {
+                            return if (entry == null) "" else decimalFormat.format(entry.y)
+                        }
+                    }
+                } */
+                val pieData = PieData(pieDataSet)
+
+                categorySpendingChart.data = pieData
+                /* categorySpendingChart.xAxis.apply {
+                    position = XAxis.XAxisPosition.BOTTOM
+                    granularity = 1f
+                    valueFormatter = IndexAxisValueFormatter(labels)
+                    labelRotationAngle = -45f
+                    setDrawGridLines(false)
+                } */
+
+                categorySpendingChart.notifyDataSetChanged()
+                categorySpendingChart.invalidate()
+            } else {
+                categorySpendingChart.clear()
+                categorySpendingChart.setNoDataText("No spending data for this period")
+                categorySpendingChart.invalidate()
+            }
+        }
+    }
 }
