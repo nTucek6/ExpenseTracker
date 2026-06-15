1e121244fa9b1a53ab83ef736d810e6be1d00f28
nTucek6 <lordd4193@gmail.com>
2026-06-15 11:32:37 +0200

    Fix DailyBudgetSpent query

--- app/src/main/java/com/example/expensetracker/data/model/DailyBudgetSpent.kt
+++ app/src/main/java/com/example/expensetracker/data/model/DailyBudgetSpent.kt
@@ -2,14 +2,12 @@ package com.example.expensetracker.data.model
 
 import androidx.room.DatabaseView
 
-//        ((e.createdAt / 1000) / 86400) * 86400 * 1000 AS date,
-//GROUP BY (e.createdAt / 1000) / 86400
 @DatabaseView("""
     SELECT
-    CAST(strftime('%Y-%m-%d', datetime(e.createdAt/1000, 'unixepoch', 'localtime'))AS TEXT) AS date,
+    ((e.createdAt / 1000) / 86400) * 86400 * 1000 AS date,
         COALESCE(SUM(e.amount), 0) AS amount
     FROM expenses e
-    Group By date
+    Group By (e.createdAt / 1000) / 86400
     ORDER BY date ASC
 """)
 data class DailyBudgetSpent(
